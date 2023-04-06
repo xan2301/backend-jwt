014b64ada634d83de915f365587987ead7bc265e
xan2301 <2silverghost2@gmail.com>
2023-04-06 22:53:37 +0200

    First commit

---
+++ src/test/java/pl/wiktorowski/backendjwt/BackendJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wiktorowski.backendjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
