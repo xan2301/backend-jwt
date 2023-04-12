ea8df7da7a8b6dc0cd78dc34bcf2173d6b299f1d
xan2301 <2silverghost2@gmail.com>
2023-04-12 15:14:36 +0200

    Add JWT 20.00

--- src/main/java/pl/wiktorowski/backendjwt/user/UserApi.java
+++ src/main/java/pl/wiktorowski/backendjwt/user/UserApi.java
@@ -1,9 +1,14 @@
 package pl.wiktorowski.backendjwt.user;
 
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,22 +24,34 @@ public class UserApi {
     }
 
     @PostMapping("/auth/login")
-    public String getJwt(@RequestBody AuthRequest authRequest){
+    public ResponseEntity<?> getJwt(@RequestBody AuthRequest authRequest) {
 
 
-        Authentication authenticate = authorizationManager.authenticate(
-
-                new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
-
-        User user = (User) authenticate.getPrincipal();
-
-        System.out.println(user);
+        try {
+            Authentication authenticate = authorizationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword()));
+            User user = (User) authenticate.getPrincipal();
 
 
+            Algorithm algorithm = Algorithm.HMAC256("secret");
+            String token = JWT.create()
+                    .withSubject(user.getUsername())
+                    .withIssuer("Przemo")
+                    .sign(algorithm);
 
 
 
-        return "";
+
+            AuthResponse authResponse = new AuthResponse(user.getUsername(), token);
+            return ResponseEntity.ok(authResponse);
+
+
+
+
+        } catch (UsernameNotFoundException exception) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+
+        }
     }
 
 }
