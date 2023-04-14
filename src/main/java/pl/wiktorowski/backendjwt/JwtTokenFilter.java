82e4999e9c43818a59fafa3936cc8af97e9c574f
xan2301 <2silverghost2@gmail.com>
2023-04-14 15:16:26 +0200

    Add Role

--- src/main/java/pl/wiktorowski/backendjwt/JwtTokenFilter.java
+++ src/main/java/pl/wiktorowski/backendjwt/JwtTokenFilter.java
@@ -1,20 +1,70 @@
 package pl.wiktorowski.backendjwt;
 
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
 
 @Component
 
 public class JwtTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorization = request.getHeader("Authorization");
+
+
+        if (authorization == null){
+
+            filterChain.doFilter(request, response);
+
+            return;
+        }
+
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = getUsernamePasswordAuthenticationToken(authorization);
+        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        filterChain.doFilter(request, response);
 
     }
+
+    public UsernamePasswordAuthenticationToken getUsernamePasswordAuthenticationToken(String token){
+        Algorithm algorithm = Algorithm.HMAC256("secret");
+        JWTVerifier verifier = JWT.require(algorithm)
+                .build();
+
+        DecodedJWT jwt = verifier.verify(token.substring(7));
+        Boolean isAdmin = jwt.getClaim("isAdmin").asBoolean();
+
+        String role = "ROLE_USER";
+
+        if(isAdmin){
+
+            role = "Role admin";
+                    
+
+        }
+
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(role);
+
+        return new UsernamePasswordAuthenticationToken(jwt.getSubject(), null, Collections.singleton(simpleGrantedAuthority));
+    }
+
+
+
 }
